002e6281c643025c3de767490b2a72dfc814693a
jardx666 <jardx66@gmail.com>
2023-10-24 12:34:46 -0600

    Proyectos Rust basados en los retos de MoureDev

---
+++ lenguaje_hacker/target/debug/deps/lenguaje_hacker-193603a5f2aa8e0d.d
@@ -0,0 +1,5 @@
+/Users/jardx66/Documents/Rust/lenguaje_hacker/target/debug/deps/lenguaje_hacker-193603a5f2aa8e0d.rmeta: src/main.rs
+
+/Users/jardx66/Documents/Rust/lenguaje_hacker/target/debug/deps/lenguaje_hacker-193603a5f2aa8e0d.d: src/main.rs
+
+src/main.rs:
---
+++ lenguaje_hacker/target/debug/deps/lenguaje_hacker-2f67d29cbb6e5d70.d
@@ -0,0 +1,5 @@
+/Users/jardx66/Documents/Rust/lenguaje_hacker/target/debug/deps/lenguaje_hacker-2f67d29cbb6e5d70.rmeta: src/main.rs
+
+/Users/jardx66/Documents/Rust/lenguaje_hacker/target/debug/deps/lenguaje_hacker-2f67d29cbb6e5d70.d: src/main.rs
+
+src/main.rs:
